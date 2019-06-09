79e61a104dce9847801471627e49fd2c2fb4083a
Jesse Berman <jesse.ben.berman@gmail.com>
2019-06-09 16:55:00 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,7 +1,7 @@
 # code your #valid_move? method here
 def valid_move? 
   if (position_taken? = false && board[0] || board[8])
-    puts ""
+    puts "invalid"
 end
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
